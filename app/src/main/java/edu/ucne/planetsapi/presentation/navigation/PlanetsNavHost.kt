b77cc954fcffcb5e624c84ffef24c1e824eed55f
FelixAlbertoM <af301509@gmail.com>
2026-06-20 19:17:41 -0400

    crea PlanetsNavHost.kt y Screen.kt

---
+++ app/src/main/java/edu/ucne/planetsapi/presentation/navigation/PlanetsNavHost.kt
@@ -0,0 +1,32 @@
+package edu.ucne.planetsapi.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import edu.ucne.planetsapi.presentation.list.ListPlanetScreen
+import edu.ucne.planetsapi.presentation.details.PlanetDetailScreen
+
+@Composable
+fun PlanetsNavHost(navHostController: NavHostController) {
+    NavHost(
+        navController = navHostController,
+        startDestination = Screen.PlanetList
+    ) {
+        composable<Screen.PlanetList> {
+            ListPlanetScreen(
+                goToPlanet = { id ->
+                    navHostController.navigate(Screen.PlanetDetail(id))
+                }
+            )
+        }
+        composable<Screen.PlanetDetail> {
+            val args = it.toRoute<Screen.PlanetDetail>()
+            PlanetDetailScreen(
+                planetId = args.planetId,
+                onNavigateBack = { navHostController.navigateUp() }
+            )
+        }
+    }
+}
