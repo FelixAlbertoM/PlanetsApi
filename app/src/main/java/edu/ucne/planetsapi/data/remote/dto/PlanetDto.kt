9b6b26e3b41d264bca44d09038d7ffa6ff670f22
FelixAlbertoM <af301509@gmail.com>
2026-06-20 18:11:28 -0400

    crea PlanetDto.kt

---
+++ app/src/main/java/edu/ucne/planetsapi/data/remote/dto/PlanetDto.kt
@@ -0,0 +1,23 @@
+package edu.ucne.planetsapi.data.remote.dto
+
+import edu.ucne.planetsapi.domain.model.Planet
+
+data class PlanetsResponse(
+    val items: List<PlanetDto>
+)
+
+data class PlanetDto(
+    val id: Int,
+    val name: String,
+    val isDestroyed: Boolean,
+    val description: String,
+    val image: String
+) {
+    fun toDomain() = Planet(
+        id = id,
+        name = name,
+        isDestroyed = isDestroyed,
+        description = description,
+        image = image
+    )
+}
