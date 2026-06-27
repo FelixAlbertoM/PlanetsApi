849a4b1ce5984f08f1bb898fac98276aad512649
FelixAlbertoM <af301509@gmail.com>
2026-06-27 17:06:57 -0400

    Modifica DragonBallApi.kt

--- app/src/main/java/edu/ucne/planetsapi/data/remote/DragonBallApi.kt
+++ app/src/main/java/edu/ucne/planetsapi/data/remote/DragonBallApi.kt
@@ -2,6 +2,8 @@ package edu.ucne.planetsapi.data.remote
 
 import edu.ucne.planetsapi.data.remote.dto.PlanetDto
 import edu.ucne.planetsapi.data.remote.dto.PlanetResponseDto
+import edu.ucne.planetsapi.data.remote.dto.CharacterResponseDto
+import edu.ucne.planetsapi.data.remote.dto.CharacterDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -21,4 +23,18 @@ interface DragonBallApi {
     suspend fun getPlanetDetail(
         @Path("id") id: Int
     ):  Response<PlanetDto>
+
+    @GET("characters")
+    suspend fun getCharacters(
+        @Query("page") page: Int,
+        @Query("limit") limit: Int,
+        @Query("name") name: String?,
+        @Query("gender") gender: String?,
+        @Query("race") race: String?
+    ): Response<CharacterResponseDto>
+
+    @GET("characters/{id}")
+    suspend fun getCharacterDetail(
+        @Path("id") id: Int
+    ): Response<CharacterDto>
 }
